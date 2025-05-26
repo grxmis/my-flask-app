901bcb9a472b3f3b9aedae7c3088a9aaa487ecf6
grxmis <xmiserlis@yahoo.gr>
2025-05-26 16:06:16 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Γεια σου κόσμε από Flask στο Render!'
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    message = ""
+    if request.method == 'POST':
+        if request.form.get('action') == 'hello':
+            message = "Γεια σου κόσμε!"
+        elif request.form.get('action') == 'goodbye':
+            message = "Αντίο φίλε μου!"
+    return render_template('index.html', message=message)
 
 if __name__ == '__main__':
     app.run()
